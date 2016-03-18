308a5eaad1ee48fa521865104f3dd5ef027ae7ed
John Flavin <flavinj@gmail.com>
2016-03-18 15:13:33 -0500

    Make Transporter an XNAT module

--- src/main/java/org/nrg/transporter/config/TransporterConfig.java
+++ src/main/java/org/nrg/transporter/config/TransporterConfig.java
@@ -1,9 +1,11 @@
 package org.nrg.transporter.config;
 
+import org.nrg.framework.annotations.XnatModule;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@XnatModule(value = "transporter", description = "XNAT Transporter", config = TransporterConfig.class)
 @ComponentScan("org.nrg.transporter")
 public class TransporterConfig {
 }

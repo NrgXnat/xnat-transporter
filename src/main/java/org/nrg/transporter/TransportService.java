f720c7d4511cea0af22dd673550976ebe01ab880
John Flavin <flavinj@gmail.com>
2016-03-21 13:23:26 -0500

    Still bad, but now it is not guaranteed to fail.

--- src/main/java/org/nrg/transporter/TransportService.java
+++ src/main/java/org/nrg/transporter/TransportService.java
@@ -1,12 +1,13 @@
 package org.nrg.transporter;
 
+import org.nrg.xdat.om.base.BaseXnatExperimentdata.UnknownPrimaryProjectException;
 import org.nrg.xft.XFTItem;
 
 import java.nio.file.Path;
 import java.util.List;
 
 public interface TransportService {
-    List<Path> transport(String destinationName, XFTItem... items);
+    List<Path> transport(String destinationName, XFTItem... items) throws UnknownPrimaryProjectException;
 
     List<Path> transport(String destinationName, Path... file);
 
--- src/main/java/org/nrg/transporter/TransportServiceImpl.java
+++ src/main/java/org/nrg/transporter/TransportServiceImpl.java
@@ -2,26 +2,26 @@ package org.nrg.transporter;
 
 import com.google.common.collect.Lists;
 import org.nrg.xdat.om.XnatMrsessiondata;
+import org.nrg.xdat.om.base.BaseXnatExperimentdata.UnknownPrimaryProjectException;
 import org.nrg.xft.XFTItem;
-import org.nrg.xnat.turbine.utils.ArcSpecManager;
 import org.springframework.stereotype.Service;
 
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 @Service
 public class TransportServiceImpl implements TransportService {
     @Override
-    public List<Path> transport(final String destinationName, XFTItem... items) {
+    public List<Path> transport(final String destinationName, XFTItem... items) throws UnknownPrimaryProjectException {
+        List<Path> paths = Lists.newArrayList();
         for (final XFTItem item : items) {
             if (item.getXSIType().equals("xnat:mrSessionData")) {
                 final XnatMrsessiondata session = new XnatMrsessiondata(item);
-
-                ArcSpecManager.GetInstance().getArchivePathForProject(session.getProject());
-                session.getFileResources()
+                    paths.add(Paths.get(session.getArchivePath()));
             }
         }
-        return null;
+        return paths;
     }
 
     @Override
